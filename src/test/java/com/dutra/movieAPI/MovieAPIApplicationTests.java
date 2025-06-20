c2c842cadb73fb1c77694c4f46ad89927cc871fa
luis.dutra <luis.dutra@softfocus.com.br>
2025-06-20 12:19:36 -0300

    alterado nome do teste

---
+++ src/test/java/com/dutra/movieAPI/MovieAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutra.movieAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAPIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
